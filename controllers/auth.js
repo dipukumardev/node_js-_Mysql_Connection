cde1642e88d433f10e894c5835e87d43389a933a
dipu kumar <dipukumardevcod@email.com>
2025-02-03 02:09:55 +0530

    update the code

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,61 @@
-exports.register = (req, res)=>{
+const mysql = require('mysql');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+
+exports.register = (req, res) => {
     console.log(req.body);
-    res.send("Form Submitted")
+
+    // const name = req.body.name;
+    // const email = req.body.email;
+    // const password = req.body.password;
+    // const passwordconfirm = req.body.passwordconfirm;
+
+
+    const { name, email, password, passwordconfirm } = req.body;
+
+    db.query('SELECT email FROM users WHERE email = ?', [email], async (error, results) => {
+        if (error) {
+            console.log(error);
+        }
+
+        if (results.length > 0) {
+            return res.render('register', {
+                message: 'That email is already in use'
+            })
+        }
+
+        else if (password !== passwordconfirm) {
+            return res.render('register', {
+                message: 'Password do not match'
+            });
+        }
+
+        let heshedPassword = await bcrypt.hash(password, 8);
+        console.log(heshedPassword);
+
+        db.query('INSERT INTO users SET ?', { name: name, email: email, password: heshedPassword }, (error, results) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(results);
+                return res.render('register', {
+                    message: 'User Registerd'
+                })
+            }
+        })
+    });
+
+
+
+
+
+
 }
